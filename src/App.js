da3f98b3d925b3524a0923246b55b1415d4a84f3
julandrod <julian.andres.rodriguez@gmail.com>
2021-05-29 23:47:58 -0500

    finish header component inside chat component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import Sidebar from './components/Sidebar';
+import Sidebar from './components/sidebar/Sidebar';
+import Chat from './components/chat/Chat';
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
+      <Chat />
     </div>
   );
 }
