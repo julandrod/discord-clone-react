518f04f39db525c2a3c932c2fb1ef048f532fc5c
julandrod <julian.andres.rodriguez@gmail.com>
2021-05-29 22:00:28 -0500

    making sidebar component

---
+++ src/components/Sidebar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Sidebar.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import SidebarChannel from "./SidebarChannel";
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar_top">
+        <h3>Madness</h3>
+        <ExpandMoreIcon />
+      </div>
+
+      <div className="sidebar_channels">
+        <div className="sidebar_channelsHeader">
+          <div className="sidebar_header">
+            <ExpandMoreIcon />
+            <h4>Text Channels</h4>
+          </div>
+          <AddIcon className="sidebar_addChannel" />
+        </div>
+
+        <div className="sidebar_channelsList">
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+          <SidebarChannel />
+        </div>
+      </div>
+
+      <div className="sidebar_voice">
+        <SignalCellularAltIcon className="sidebar_voiceIcon" fontSize="large" />
+        <div className="sidebar_voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
+        </div>
+
+        <div className="sidebar_voiceIcons">
+          <InfoOutlinedIcon />
+          <CallIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/SidebarChannel.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SidebarChannel.css";
+
+const SidebarChannel = () => {
+  return (
+    <div className="sidebarChannel">
+      <h4>
+        <span className="sidebarChannel_hash">#</span>Youtube
+      </h4>
+    </div>
+  );
+};
+
+export default SidebarChannel;
